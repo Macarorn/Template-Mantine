513560490d834a253da24412938ea1e056b7eefc
Mariana <marianacifuenteszu@gmail.com>
2023-08-03 17:20:29 -0500

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import { Button, Group } from "@mantine/core";
 export default function IndexPage() {
   return (
     <Group mt={50} position="center">
-      <Button size="xl">Welcome to Mantine!</Button>
+      <Button size="xl">Meow 1</Button>
+      <Button size="xl">Meow 2</Button>
+      <Button size="xl">Meow 3</Button>
     </Group>
   );
 }
